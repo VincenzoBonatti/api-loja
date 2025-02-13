7ba0ab02feed3822781096ac195624ebe5627a04
Vincenzo Bonatti <vincenzobonati@outlook.com>
2025-02-13 00:24:52 -0300

    primeiro post e get

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,21 @@
-import { FastifyInstance } from "fastify";
+import { randomUUID } from "node:crypto";
 import { FastifyTypedInstance } from "./types";
 import z from "zod";
 
+
+interface User {
+    id: string,
+    name: string,
+    email: string
+}
+
+const users: User[] = []
+
+
+
 export async function routes(app:FastifyTypedInstance) {
     app.get("/users", () =>{
-        return "aaaaaa"
+        return users
     })
 
 
@@ -18,7 +29,17 @@ export async function routes(app:FastifyTypedInstance) {
                 
             })
         }
-    }, () =>{
-        return
+    }, (request, reply) =>{
+
+        const{ name, email } = request.body
+
+        users.push({
+            id:randomUUID(),
+            name,
+            email
+        })
+
+
+        return reply.status(201).send()
     })
 }   
