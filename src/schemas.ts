7e68d0cfd51a19bce8d93856a0d585dd2f17887c
Vincenzo Bonatti <vincenzobonati@outlook.com>
2025-03-18 11:38:10 -0300

    bugfix

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,105 +1,104 @@
 import z from "zod";
 
-    export const schemas = {
+export const schemas = {
 
-        getUserId: {
-            schema: {
-                tags: ["users"],
-                description: "get user by name",
-                response: {
-                    200: z.object({
-                        id: z.string(),
-                        name: z.string(),
-                        email: z.string(),
-                        senha: z.string(),
-                    })
-                }
-            }
-        },
-
-        getUsers: {
-            schema: {
-                tags: ["users"],
-                description: "get all users",
-                response: {
-                    200: z.array(z.object({
-                        id: z.string(),
-                        name: z.string(),
-                        email: z.string(),
-                        senha: z.string(),
-                    }))
-                }
-            }
-        },
-
-        postUser: {
-            schema: {
-                tags: ["users"],
-                description: "create a new user",
-                body: z.object({
+    getUserId: {
+        schema: {
+            tags: ["users"],
+            description: "get user by name",
+            response: {
+                200: z.object({
+                    id: z.string(),
                     name: z.string(),
-                    email: z.string().email(),
-                    senha: z.string()
-                    
-                }),
-                response: {
-                    200: z.null().describe("user created"),
-                    
-                },
+                    email: z.string(),
+                    senha: z.string(),
+                })
+            }
+        }
+    },
+
+    getUsers: {
+        schema: {
+            tags: ["users"],
+            description: "get all users",
+            response: {
+                200: z.array(z.object({
+                    id: z.string(),
+                    name: z.string(),
+                    email: z.string(),
+                    senha: z.string(),
+                }))
+            }
+        }
+    },
+
+    postUser: {
+        schema: {
+            tags: ["users"],
+            description: "create a new user",
+            body: z.object({
+                name: z.string(),
+                email: z.string().email(),
+                senha: z.string()
+
+            }),
+            response: {
+                200: z.null().describe("user created"),
+
             },
         },
+    },
 
-        getProducts: {
-            schema: {
-                tags: ["products"],
-                description: "get all products",
-                response: {
-                    200: z.array(z.object({
-                        id: z.string(),
-                        name: z.string(),
-                        estoque: z.number(),
-                        preco: z.number(),
-                    }))
-                }
-            }
-        },
-
-        postProducts: {
-            schema: {
-                tags: ["products"],
-                description: "create a new product",
-                body: z.object({
+    getProducts: {
+        schema: {
+            tags: ["products"],
+            description: "get all products",
+            response: {
+                200: z.array(z.object({
+                    id: z.string(),
                     name: z.string(),
-                    estoque: z.number().int(),
-                    preco: z.number()
-                    
-                }),
-                response: {
-                    200: z.null().describe("product created"),
-                    
-                },
+                    estoque: z.number(),
+                    preco: z.number(),
+                }))
+            }
+        }
+    },
+
+    postProducts: {
+        schema: {
+            tags: ["products"],
+            description: "create a new product",
+            body: z.object({
+                name: z.string(),
+                estoque: z.number().int(),
+                preco: z.number()
+
+            }),
+            response: {
+                200: z.null().describe("product created"),
+
             },
         },
+    },
 
-        getProductId: {
-            schema: {
-                tags: ["products"],
-                description: "get product by id",
-                response: {
-                    200: z.object({
-                        id: z.string(),
-                        name: z.string(),
-                        estoque: z.number(),
-                        preco: z.number(),
-                    })
-                }
+    getProductId: {
+        schema: {
+            tags: ["products"],
+            description: "get product by id",
+            response: {
+                200: z.object({
+                    id: z.string(),
+                    name: z.string(),
+                    estoque: z.number(),
+                    preco: z.number(),
+                })
             }
-        },
+        }
+    },
 
-    }
+}
 
-     
 
-    
 
-    
+
+
--- src/service.ts
+++ src/service.ts
@@ -1,5 +1,6 @@
 import { randomUUID } from "node:crypto";
 
+
 interface User {
     id: string,
     name: string,
@@ -7,7 +8,7 @@ interface User {
     senha: string
 }
 
-interface Products{
+interface Products {
     id: string,
     name: string,
     estoque: number,
@@ -19,35 +20,35 @@ export const users: User[] = []
 
 export function userPeloNome(request, reply) {
     let verificador = users.find(el => el.name === request.params.name)
-            
-            if(!verificador){
-                return reply.status(404).send()
-            } else if (verificador){
-                reply.send(verificador)
-            }
+
+    if (!verificador) {
+        return reply.status(404).send()
+    } else if (verificador) {
+        reply.send(verificador)
+    }
 }
 
-export  function cadastrarUsuario(request, reply) {
-    const {name, email, senha} = request.body
-        let verificador = users.find(el => el.email === email)
-        if(!verificador){          
-            users.push({
-                id:randomUUID(),
-                name, 
-                email,
-                senha
-            })
-            return reply.status(200).send()
-        } else if (verificador){
-            return reply.status(409).send()
-        }
+export function cadastrarUsuario(request, reply) {
+    const { name, email, senha } = request.body
+    let verificador = users.find(el => el.email === email)
+    if (!verificador) {
+        users.push({
+            id: randomUUID(),
+            name,
+            email,
+            senha
+        })
+        return reply.status(200).send()
+    } else if (verificador) {
+        return reply.status(409).send()
+    }
 }
 
-export  function cadastrarProduto(request, reply) {
-    const {name, estoque, preco} = request.body   
+export function cadastrarProduto(request, reply) {
+    const { name, estoque, preco } = request.body
     products.push({
-        id:randomUUID(),
-        name, 
+        id: randomUUID(),
+        name,
         estoque,
         preco
     })
@@ -56,10 +57,10 @@ export  function cadastrarProduto(request, reply) {
 
 export function productsById(request, reply) {
     let verificador = products.find(el => el.id === request.params.id)
-            
-            if(!verificador){
-                return reply.status(404).send()
-            } else if (verificador){
-                reply.send(verificador)
-            }
+
+    if (!verificador) {
+        return reply.status(404).send()
+    } else if (verificador) {
+        reply.send(verificador)
+    }
 }
